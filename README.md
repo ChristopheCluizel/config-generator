2db40317b213a8cec289ee15a6baa8f0adfd644a
Christophe Cluizel <christophe.cluizel@gmail.com>
2018-05-20 17:37:37 +0200

    Create deploy script with Ansible

--- README.md
+++ README.md
@@ -33,8 +33,19 @@ The container will be run in attached mode to be able to directly see the result
 
 ## CURL query examples
 ```bash
-* curl -X POST -H "Content-Type: text/plain" --data-binary '{"key":"value"}' 'localhost:8000/generate-conf?key_prefix=perso/dev'
+* curl -X POST -H "Content-Type: text/plain" --data-binary '{"key":"{{all_test_config__generator_test__key}}"}' 'localhost:8000/generate-conf'
 * curl -X POST -H "Content-Type: text/plain" --data-binary @resources/conf_template_examples/json_conf_template.j2  'localhost:8000/generate-conf?key_prefix=perso/dev'
 * cat resources/conf_template_examples/python_conf_template.j2 | curl -X POST -H "Content-Type: text/plain" --data-binary @- 'localhost:8000/generate-conf?key_prefix=perso/dev'
 
+```
+
+## How to deploy
+
+The following points are required on the machine before deploying:
+* attach a role to the machine so that it can access the Parameter Store.
+* attach a security group to the machine to whitelist IPs. Otherwise, the API will be opened to the world and all your Parameter Store secrets reachable by anyone. Authorize only HTTPS connection.
+
+Currently, the following deploy script works only for an instance with the following Amazon AMI : ami-66506c1c
+```bash
+./scripts/deploy.sh --help
 ```
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,58 @@
 #!/usr/bin/env bash
 
 set -e
+
+function usage() {
+    echo "Deploy the application to a host machine
+
+Usage: $0 [arguments]
+    -h, --help                      display this help message
+    --host-ip        <HOST_IP>      the public IP of a host machine to deploy
+
+Example:
+    ./deploy.sh --host-ip 192.0.0.1
+"
+}
+
+# Check that we run this script from cj-rocket root folder
+CURRENT_DIRECTORY=$( basename "$PWD" )
+
+if [ "$CURRENT_DIRECTORY" != "config_generator" ]; then
+    echo "You must run this script from config_generator root project directory."
+    exit 1
+fi
+
+# Retrieve project path
+DIR="$( pwd )"
+
+# Load helpers
+source "${DIR}/scripts/functions.sh"
+
+# get input arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --host-ip)
+        HOST_IP="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -h|--help)
+        usage
+        exit 0
+        ;;
+        *)
+        unknown_argument "$@"
+        ;;
+    esac
+done
+
+# Validate mandatory arguments
+check_argument "host-ip" "${HOST_IP}"
+
+# Set the remote host IP in the Ansible inventory file
+sed "s/<host_ip>/""$HOST_IP""/g" inventory.ini.template > inventory.ini
+
+# Provide the host machine
+ansible-playbook ansible.yml -i inventory.ini
